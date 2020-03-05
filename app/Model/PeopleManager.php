dce9f9a5ba026dca2d5740506d6122c1d32efcdc
QuakyCZ <kvaky.kratschmer@gmail.com>
2020-03-05 19:54:25 +0100

    ikajfd

--- app/Model/PeopleManager.php
+++ app/Model/PeopleManager.php
@@ -33,8 +33,22 @@ final class PeopleManager
         ]);
     }
 
-    public function deleteRowWhere(string $column,$value)
+    public function deleteRowWhere(string $column,string $value)
     {
         $this->database->table('people')->where($column,$value)->delete();
+    }
+
+    public function update(int $id, string $name, string $tel):void
+    {
+        $this->database->table('people')->where('name',$name)->update([
+            'name'=>$name,
+            'telnum'=>$tel
+        ]);
+    }
+
+    public function order(string $column,string $method)
+    {
+        bdump($column.', '.$method);
+        return $this->database->table('people')->order($column.' '.$method);
     }
 }
--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -13,6 +13,7 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
     /** @var PeopleManager */
     private $peopleManager;
+    private $updateId;
 
     public function __construct(PeopleManager $peopleMananger)
     {
@@ -26,12 +27,29 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
     public function handleDelete(int $id)
     {
-        $this->peopleManager->deleteRowWhere('id',$id);
+        $this->peopleManager->deleteRowWhere('id',strval($id));
         //$this->template->people = $this->peopleManager->getPeople();
         if($this->isAjax()){
-            $this->redrawAll();
+            $this->redrawControl();
             bdump('redrawed delete');
         }
+    }
+
+    public function handleUpdate(int $id):void
+    {
+        bdump($id);
+        $this->updateId=$id;
+        bdump($this->updateId);
+        $this->template->updateId = $id;
+        $this->redrawControl();
+    }
+
+    public function handleOrder(string $value):void
+    {
+        $order = $this->peopleManager->order($value, 'ASC');
+        bdump($order);
+        $this->template->people = $order;
+        $this->redrawControl('table');
     }
 
     public function createComponentAddForm():Form
@@ -44,33 +62,53 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
         return $form;
     }
 
+    public function createComponentUpdateForm():Form
+    {
+        $form = new Form;
+        $form->addInteger('id','Id');
+        $form->addText('name','Name')->setRequired('name is required');
+        $form->addText('tel', 'Tel')->setRequired('tel is required');
+        $form->addSubmit('submit', 'Update');
+        $form->onSuccess[] = [$this,'onUpdateFormSucceeded'];
+        return $form;
+    }
+
     public function onAddFormSucceeded(Form $form, \stdClass $values):void
     {
         $existing = $this->peopleManager->getPeopleWhere('name',$values->name);
-        if(empty($existing)){
+        if(empty($existing) || $existing->id==$values->id){
             $this->peopleManager->insertRow($values->name,$values->tel);
             bdump('successfuly inserted');
         }
         else{
-            $form->addError('Name exists');
+            $this->flashMessage('Name exists');
             bdump('name exists');
         }
         //$this->template->people = $this->peopleManager->getPeople();
         if($this->isAjax()){
-            $this->redrawAll();
+            $this->redrawControl();
             bdump('onAddFormSucceeded redrawed');
         }
         else{
             bdump('no ajax');
+        }    
+    }
+
+    public function onUpdateFormSucceeded(Form $form, \stdClass $values):void
+    { 
+        $existing = $this->peopleManager->getPeopleWhere('name',$values->name);
+        if(!isset($existing)){          
+            $this->peopleManager->update($values->id,$values->name,$values->tel);
         }
-
-    
+        else{
+            $this->flashMessage('Name exists');
+        }
+        if($this->isAjax()){
+            $this->redrawControl('table');
+        }
+        else{
+            $this->redirect('Homepage:default');
+        }
+        
     }
-
-    private function redrawAll()
-    {
-        $this->redrawControl('table');
-        $this->redrawControl('form');
-    }
-
 }
