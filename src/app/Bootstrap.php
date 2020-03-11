e542afe7ad9256bf7afc6dbd9d85758ddb87e1a5
QuakyCZ <kvaky.kratschmer@gmail.com>
2020-03-11 17:41:51 +0100

    dfjndo

--- src/app/Bootstrap.php
+++ src/app/Bootstrap.php
@@ -15,7 +15,7 @@ class Bootstrap
 
 		//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 		$configurator->enableTracy(__DIR__ . '/../log');
-
+if (!empty($_SERVER['DOCKER'])) { $configurator->setDebugMode(true); }
 		$configurator->setTimeZone('Europe/Prague');
 		$configurator->setTempDirectory(__DIR__ . '/../temp');
 
